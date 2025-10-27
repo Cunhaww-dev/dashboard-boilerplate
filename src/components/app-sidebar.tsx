fd990bebd1c36e1b90fab980f56d918d6b3a43f1
Cunhaww-dev <dev.cunhaww@gmail.com>
2025-10-27 15:59:22 -0300

    feat: install lucid icons and use new icon

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -9,6 +9,7 @@ import {
   LifeBuoy,
   Map,
   PieChart,
+  ScrollText,
   Send,
   Settings2,
   SquareTerminal,
@@ -105,7 +106,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
                 <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <Command className="size-4" />
+                  <ScrollText className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">Attendence List</span>
